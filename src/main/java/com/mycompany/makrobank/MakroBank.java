268709b7472065ca76ceb6bb0c36f1a258a89956
matheus <matheusdeoliveiramota2020@gmail.com>
2026-06-18 21:09:07 -0300

    first commit

---
+++ src/main/java/com/mycompany/makrobank/MakroBank.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.makrobank;
+
+/**
+ *
+ * @author matheus
+ */
+public class MakroBank {
+
+    public static void main(String[] args) {
+        
+    }
+}
