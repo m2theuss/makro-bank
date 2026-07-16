ab9ff8c2c6687b9196b8e1de18c6f85776a0351a
matheus <matheusdeoliveiramota2020@gmail.com>
2026-07-15 21:59:02 -0300

    wip: refactor login() and create() methods

--- src/main/java/com/mycompany/makrobank/view/AuthView.java
+++ src/main/java/com/mycompany/makrobank/view/AuthView.java
@@ -9,7 +9,7 @@ import com.mycompany.makrobank.model.domain.User;
 import com.mycompany.makrobank.security.TokenService;
 public class AuthView {
     private final Scanner scan; 
-    private final String SHOW_PASSWORD_WHILE_TYPING = "Do you want hide your password while typing (Y or N)? ";
+    private final String HIDDEN_PASSWORD = "Do you want hide your password while typing (Y or N)? ";
     public AuthView(Scanner scan){
         this.scan = scan;
     }
@@ -79,20 +79,7 @@ public class AuthView {
     private boolean create(){ 
         System.out.println("Let's create an account!");
         String name = readName();
-        String password = null;
-        while(true){
-            System.out.print(SHOW_PASSWORD_WHILE_TYPING);
-            String decision = scan.nextLine().toUpperCase();
-            if("Y".equals(decision)){
-                password = readHiddenPassword();
-                break;
-            }else if("N".equals(decision)){
-                password = readPassword();
-                break;
-            }else{
-                System.out.println("Invalid option, try again.");
-            }
-        }
+        String password = readPasswordByChoice(() -> readPassword(), () -> readHiddenPassword());
         if(password == null){
             return false;
         }
@@ -102,51 +89,67 @@ public class AuthView {
         return controller.create(newUser);
     }
     private User login(){
-        System.out.print("Type the user: ");
-        String name = scan.nextLine();
-        System.out.print(SHOW_PASSWORD_WHILE_TYPING);
-        String showPassword = scan.nextLine().toUpperCase();
-        String password = null;
-        while(true){
-            if("Y".equals(showPassword)){
-                System.out.print("Type (won't show): ");
-                password = readHiddenInput();
-                break;
-            }else if("N".equals(showPassword)){
-                System.out.print("Type: ");
-                password = scan.nextLine();
-                break;
+        AuthController controller = new AuthController();
+        for(int i = 3; i < 0; i--){
+            String name;
+            while(true){
+                System.out.print("Type the user: ");
+                name = nameValidation(scan.nextLine());
+                if(name == null){
+                    break;
+                }   
+                System.out.println(name);
+            }
+            String password = readPasswordByChoice(() -> scan.nextLine(), () -> readSecureInput()); 
+            User result = controller.login(name,password);
+            if(result == null){
+                System.out.println("Username or password incorrect, try again.");
             }
         }
-        AuthController controller = new AuthController();
-        return controller.login(name,password);
+        System.out.println("Your has been blocked, try again or later.");
+        return null;
     }
     private String readName(){
         String nameVerificationResult = null;
         String name = null;
+        System.out.print("Type name: ");
         while(true){
-            System.out.print("Type name: ");
             name = scan.nextLine();
-            nameVerificationResult = validateName(name);
+            nameVerificationResult = nameValidation(name);
             if(nameVerificationResult == null){
+                boolean nameExist = new AuthController().nameUserExist(name);
+                if(nameExist){
+                    System.out.print("This name already exist, type other: ");
+                }
                 return name;
             }
             System.out.println(nameVerificationResult);
             continue;
         }
     }
-    private String validateName(String name){
+    private String nameValidation(String name){
         if(name.isEmpty()){
             return "Empty names are not accepted.";
         }else if(name.length() > 30){
             return "Write a name with at most 30 letters.";
         }else if(name.contains(" ") || name.contains(".")){
             return "The name dont must contain spaces or points.";
-        }else if(new AuthController().nameUserExist(name)){
-            return "This name already exists, try again "
-                            + "with other name.";
         }else{
             return null;
+        }
+    }
+    public String readPasswordByChoice(Supplier<String> plainPassword, Supplier <String> hiddenPassword){
+        System.out.print("Do you want hide your password while typing (Y / N)? ");
+        while(true){
+            String choise = scan.nextLine();
+            if("n".equalsIgnoreCase(choise)){
+                System.out.print("Type: ");
+                return plainPassword.get();
+            }else if("y".equalsIgnoreCase(choise)){
+                System.out.print("Type (won't show): ");
+                return hiddenPassword.get();
+            }
+            System.out.print("Write a valid option (Y / N): ");
         }
     }
     private String readPassword(){
@@ -157,7 +160,7 @@ public class AuthView {
     }
     private String readHiddenPassword(){
         return genericPasswordReader(
-            () -> readHiddenInput(),
+            () -> readSecureInput(),
             () -> System.out.print("Type (won't show): ")
         );
     }
@@ -167,20 +170,19 @@ public class AuthView {
             String password = supplier.get();
             String error = validatePassword(password);
             if(error == null){
-                for(int i = 3; i >= 0; i--){
-                    System.out.print("Type again to confirm your password: ");
+                System.out.print("Type again to confirm your password: ");
+                while(true){
                     if(password.equals(supplier.get())){
                         return password;
                     }
-                    System.out.println("The password do not match, try again (you have " + i + " more attempts)." );
+                    System.out.print("The password do not match, type again: " );
                 }
-                return null;
             }else{
                 System.out.println(error);
             }
         }
     }
-    public String readHiddenInput(){
+    public String readSecureInput(){
         Console console = System.console();
         if(console == null){
             System.out.println("Warning: The program cannot find a console, which means that it's running in an IDE. Please run it in a simple terminal.");
